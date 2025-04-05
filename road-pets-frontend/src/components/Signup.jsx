5f3b716db81ea571a968c0725eb3e71ae273b080
Tdmaxer <ytdilshan07@gmail.com>
2025-04-05 16:54:19 +0530

    Update Signup.jsx

--- road-pets-frontend/src/components/Signup.jsx
+++ road-pets-frontend/src/components/Signup.jsx
@@ -12,7 +12,7 @@ function Signup() {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            await axios.post('http://localhost:5000/api/auth/register', {
+            await axios.post('https://roadpets.onrender.com/api/auth/register', {
                 username,
                 email,
                 password,
