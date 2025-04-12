8749525ff50faa51063a94024c97c74029b79959
Tdmaxer <ytdilshan07@gmail.com>
2025-04-12 20:41:30 +0530

    Update Navbar.jsx

--- road-pets-frontend/src/components/Navbar.jsx
+++ road-pets-frontend/src/components/Navbar.jsx
@@ -3,6 +3,11 @@ import Logo from "../asserts/logo.png";
 import '../CSS/navbar.css';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import { useNavigate } from 'react-router-dom';
+import PetsIcon from '@mui/icons-material/Pets';
+import CollectionsIcon from '@mui/icons-material/Collections';
+import PagesIcon from '@mui/icons-material/Pages';
+import HomeIcon from "@mui/icons-material/Home";
+
 
 const Navbar = () => {
   const isAuthenticated = localStorage.getItem('token') !== null;
@@ -36,23 +41,38 @@ const Navbar = () => {
         <div className="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
           <ul className="navbar-nav mb-2 mb-lg-0">
             <li className="nav-item">
-              <a className="nav-link nav-link-large" href="/">Home</a>
+              <a className="nav-link nav-link-large" href="/">
+              <HomeIcon style={{ fontSize: '24px', verticalAlign: 'middle', marginRight: '5px' }} />
+              Home</a>
             </li>
             
             {isAuthenticated && (
-              <li className="nav-item">
-                <a className="nav-link nav-link-large" href="/create">Create Post</a>
-              </li>
+              <>
+                <li className="nav-item">
+                  <a className="nav-link nav-link-large" href="/create">
+                  <PagesIcon style={{ fontSize: '24px', verticalAlign: 'middle', marginRight: '5px' }} />
+                  New Post</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link nav-link-large" href="/petdashboard">
+                    <PetsIcon style={{ fontSize: '24px', verticalAlign: 'middle', marginRight: '5px' }} />
+                    My Pets
+                  </a>
+                </li>
+              </>
             )}
             
             <li className="nav-item">
-              <a className="nav-link nav-link-large" href="/gallery">Gallery</a>
+              <a className="nav-link nav-link-large" href="/gallery">
+              <CollectionsIcon style={{ fontSize: '24px', verticalAlign: 'middle', marginRight: '5px' }} />
+              Gallery</a>
             </li>
             
             {!isAuthenticated ? (
               <>
                 <li className="nav-item px-2">
                   <a className="btn-signup" href="/signup">SignUp</a>
+                  
                 </li>
                 <li className="nav-item px-2">
                   <a className="btn-signup" href="/login">Login</a>
